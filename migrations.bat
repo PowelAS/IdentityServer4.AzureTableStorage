740aa7cad1283ac8b35a9788e41722c8ab1640b1
Tor Hovland <teh@powel.com>
2017-08-03 15:01:55 +0200

    Removed utility bat files specific for EntityFramework (not applicable for Azure Table Storage).

--- migrations.bat
+++
@@ -1,5 +0,0 @@
-cd src\Host
-rmdir /S /Q Migrations
-dotnet ef migrations add Grants -c PersistedGrantDbContext -o Migrations/IdentityServer/PersistedGrantDb
-dotnet ef migrations add Config -c ConfigurationDbContext -o Migrations/IdentityServer/ConfigurationDb
-cd ..\..
